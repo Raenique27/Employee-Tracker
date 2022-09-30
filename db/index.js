d6c9881134018cdb9018a36a34f908d9c73f8664
Raenique27 <raeniquewalters@gmail.com>
2022-09-30 09:05:48 -0400

    added function to show all employees

--- db/index.js
+++ db/index.js
@@ -1,2 +1,14 @@
+const db = require("./connection");
 
+class employeeDataBase {
+    constructor(db) {
+        this.db = db;
+    }
 
+    // Shows all employees
+    showAllEmployees() {
+        return this.db.promise().query(
+            "SELECT employee.id, employee.first_name, employee.last_name, employee.role_id, employee.manager_id FROM employee LEFT JOIN role ON employee.role_id = role.id;"
+        );
+    }
+}
