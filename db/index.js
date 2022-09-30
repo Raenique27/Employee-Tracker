c7ac8b68b43bd069a425e6f0d7780eb84333a038
Raenique27 <raeniquewalters@gmail.com>
2022-09-30 09:09:15 -0400

    added function to add an employee

--- db/index.js
+++ db/index.js
@@ -11,4 +11,13 @@ class employeeDataBase {
             "SELECT employee.id, employee.first_name, employee.last_name, employee.role_id, employee.manager_id FROM employee LEFT JOIN role ON employee.role_id = role.id;"
         );
     }
+
+     // Add an employee
+     createEmployee(firstName, lastName, role, manager) {
+        return this.db.promise().query(
+            "INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?,?,?,?);",
+            [firstName, lastName, role, manager]
+        );
+    }
+
 }
