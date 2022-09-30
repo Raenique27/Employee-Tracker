e4f409f94232facc6013ba382decd01e7c0a2ace
Raenique27 <raeniquewalters@gmail.com>
2022-09-30 08:58:27 -0400

    added values for employee table

--- db/seeds.sql
+++ db/seeds.sql
@@ -15,3 +15,14 @@ VALUES
 ('Lawyer', 190000, 3),
 ('Sales Lead', 100000, 4),
 ('Salesperson', 80000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+('Sofia', 'Lindell', 3, NULL),
+('Devin', 'Turner', 4, 1),
+('Lisa', 'Ensworth', 1, NULL),
+('Danny', 'Withers', 2, 3),
+('David', 'Silver', 5, NULL),
+('Greg', 'Delano', 6, 5),
+('Dearra', 'Williams', 7, NULL),
+('Stacy', 'Paloma', 8, 7);
