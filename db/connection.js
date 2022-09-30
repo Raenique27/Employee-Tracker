0b83e88663fece219ea29ad6677d8ef43dd54525
Raenique27 <raeniquewalters@gmail.com>
2022-09-30 08:26:33 -0400

    created and connected database

---
+++ db/connection.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql2');
+
+// Connect to database
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'rootroot',
+    database: 'employee'
+});
+
+db.connect(err => {
+    if (err) throw err;
+    console.log('Database connected.');
+});
+
+module.exports = db;
