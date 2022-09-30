c81980ada5a477975609bd4b396fa67b46dcaf43
Raenique27 <raeniquewalters@gmail.com>
2022-09-30 08:35:50 -0400

    created table for employee role

--- db/schema.sql
+++ db/schema.sql
@@ -8,3 +8,11 @@ CREATE TABLE department (
     name VARCHAR(30) NOT NULL
 );
 
+-- Table that holds employee role
+CREATE TABLE role (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER,
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id)
+);
