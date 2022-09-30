6eb7ca45fe03efc3452aa61729137b8a2042fcfd
Raenique27 <raeniquewalters@gmail.com>
2022-09-30 08:38:31 -0400

    created table for employees

--- db/schema.sql
+++ db/schema.sql
@@ -16,3 +16,14 @@ CREATE TABLE role (
     department_id INTEGER,
     CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id)
 );
+
+-- Table that holds employees
+CREATE TABLE employee (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER,
+    manager_id INTEGER,
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id),
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
